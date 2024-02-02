08b2d8c823b09a20bbe28bc12495d9c39712ee5c
Lokisvhk <chattonadrien@gmail.com>
2024-02-02 14:42:36 +0100

    Ex7OK

--- exercices/exercice7/exercice7.php
+++ exercices/exercice7/exercice7.php
@@ -1,14 +1,21 @@
 <?php
+try {
 	$bdd = new PDO('mysql:host=localhost;dbname=nomDB', 'root', 'pwd');
-	$reponse = …;
-	
-	
-	while (…)
-	{
-	
-	        …
+	// On récupère tout le contenu de la table jeux_vidéos
+	$sqlQuery = 'SELECT titre FROM jeux_video';
+	$titreJeuxStatement = $bdd->prepare($sqlQuery);
+	$titreJeuxStatement->execute();
+	$jeux = $titreJeuxStatement->fetchAll();
+} catch (Exception $e) {
+	echo '' . $e->getMessage() . '';
+}
 
-	
-	}
-	$reponse->closeCursor();
+// Si tout va bien, on peut continuer
+foreach ($jeux as $jeu) {
+	// Affichage du titre du jeu
+	echo $jeu['titre'] . '<br>';
+}
+
+// Fermeture du curseur
+$titreJeuxStatement->closeCursor();
 ?>
